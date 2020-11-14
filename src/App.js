d6174b78311bf9b1b37598b16cb152e10b72f3d9
Ajeet Singh <imajeet5@gmail.com>
2020-11-14 16:56:41 +0530

    learn basics of react

--- src/App.js
+++ src/App.js
@@ -1,12 +1,78 @@
+import React from 'react';
 import './App.css';
 
-const element = <h1>This is JSX</h1>;
+function Welcome(props) {
+  return <h1>This is a react functional component, {props.name}</h1>;
+}
+
+function FormattedDate(props) {
+  return <h2>It is {props.date.toLocaleTimeString()}.</h2>;
+}
+
+class ClockC extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { date: new Date() };
+  }
+
+  componentDidMount() {
+    this.timerId = setInterval(() => this.tick(), 1000);
+  }
+  componentWillUnmount() {
+    clearInterval(this.timerId);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date(),
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+        <FormattedDate date={this.state.date} />
+      </div>
+    );
+  }
+}
+
+function Clock(props) {
+  return (
+    <div>
+      <h1>Hello, world!</h1>
+      <h2>It is {props.date.toLocaleTimeString()}.</h2>
+    </div>
+  );
+}
+
+const element = (
+  <h1 className="greeting">This is JSX {new Date().toLocaleTimeString()}</h1>
+);
+
+const element2 = React.createElement(
+  'h1',
+  { className: 'greeting' },
+  'Hello, world!'
+);
 
 function App() {
   return (
     <div className="App">
-      Start
-      {element}
+      <div>
+        Start
+        {element2} {element}
+      </div>
+      <div>
+        <Welcome name={'Welcome'} />
+      </div>
+      <div>
+        <ClockC />
+      </div>
+      <div>
+        <Clock date={new Date()} />
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -10,6 +10,16 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+// let i = 0;
+// setInterval(() => {
+//   ReactDOM.render(
+//     <React.StrictMode>
+//       <App />
+//       <h1>{i++}</h1>
+//     </React.StrictMode>,
+//     document.getElementById('root')
+//   );
+// }, 1000);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
