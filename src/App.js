c4311aa8d7e550b354d068eed5953f1b46755d97
Ajeet Singh <imajeet5@gmail.com>
2020-11-17 15:34:09 +0530

    example to use context api

--- src/App.js
+++ src/App.js
@@ -1,12 +1,45 @@
+import React, { useState } from 'react';
 import './App.css';
 
-const element = <h1>This is JSX</h1>;
+function Toolbar(props) {
+  return (
+    <div>
+      <ThemedButton theme={props.theme} />
+    </div>
+  );
+}
+class ThemedButton extends React.Component {
+  render() {
+    return <Button theme={this.props.theme} />;
+  }
+}
+
+function Button(props) {
+  return (
+    <button className={`btn ${props.theme}`}>
+      This is a {props.theme === 'dark' ? 'Dark' : 'Light'} themed button
+    </button>
+  );
+}
 
 function App() {
+  // const [currTheme, setTheme] = useState('light');
+  const [currTheme, setTheme] = useState(() => 'light');
+
+  function changeTheme() {
+    // currTheme === 'light' ? setTheme('dark') : setTheme('light');
+    setTheme((prevTheme) => {
+      prevTheme === 'light' ? setTheme('dark') : setTheme('light');
+    });
+  }
+
   return (
     <div className="App">
-      Start
-      {element}
+      <button onClick={changeTheme} className="toggleBtn">
+        {currTheme === 'light' ? 'Light' : 'Dark'}
+      </button>
+
+      <Toolbar theme={currTheme} />
     </div>
   );
 }
