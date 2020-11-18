5f088b31f0efad29648176a973e43eb23581e393
Ajeet Singh <imajeet5@gmail.com>
2020-11-18 17:43:31 +0530

    example ref forwarding

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
+import React from 'react';
 import './App.css';
 
-const element = <h1>This is JSX</h1>;
+const ref = React.createRef();
+
+function FancyButton(props) {
+  console.log(ref);
+  function handleClick() {
+    console.log(ref);
+  }
+  console.log(props.ref);
+  return (
+    <button ref={ref} className="FancyButton" onClick={handleClick}>
+      {props.children}
+    </button>
+  );
+}
+// const FancyButton = React.forwardRef((props, reff) => {
+//   console.log(ref);
+//   function handleClick() {
+//     console.log(ref);
+//   }
+//   return (
+//     <button ref={ref} className="FancyButton" onClick={handleClick}>
+//       {props.children}
+//     </button>
+//   );
+// });
 
 function App() {
   return (
     <div className="App">
-      Start
-      {element}
+      <FancyButton>Fancy Button</FancyButton>
     </div>
   );
 }
