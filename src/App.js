365c8aa2c3aec9071cdba377c26c83e2923b3c12
Ajeet Singh <imajeet5@gmail.com>
2020-11-26 20:10:56 +0530

    added example for useEffect hook

--- src/App.js
+++ src/App.js
@@ -1,15 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
-const element = <h1>This is JSX</h1>;
+// with the useEffect we are saying that we want to do some form of side-effect, whenever something happens
 
 function App() {
-  return (
-    <div className="App">
-      Start
-      {element}
+  const [resourceType, setResourceType] = useState('posts');
+  const [items, setItems] = useState([]);
+  const [windowWidth, setwindowWidth] = useState(window.innerWidth);
+
+  const handleResize = () => {
+    setwindowWidth(window.innerWidth);
+  };
+  
+  // everytime the useEffect ran the code inside the return function will ran first do the the clean up
+
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
+      .then((response) => response.json())
+      .then((json) => setItems(json));
+  }, [resourceType]);
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  const example1 = (
+    <div>
+      <div>
+        <button onClick={() => setResourceType('posts')}>Posts</button>
+        <button onClick={() => setResourceType('users')}>Users </button>
+        <button onClick={() => setResourceType('comments')}>Comments</button>
+      </div>
+      <h1>{resourceType}</h1>
+      {items.map((item) => {
+        return <pre key={item.id}>{JSON.stringify(item)}</pre>;
+      })}
     </div>
   );
+
+  const example2 = <div>{windowWidth}</div>;
+
+  return example2;
 }
 
 export default App;
